bc850a2eef42298c7b6dfdc0e666b288b64ffce6
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-04-16 20:41:36 +0200

    normalizer, stemmer stub, number-title file generation

--- indexer.py
+++ indexer.py
@@ -28,15 +28,36 @@ def initialize_morfologik(filename):
 def generate_index(filename, compressed = False, stemmed = False):
     dirname = create_index_directory(compressed, stemmed)
     m = initialize_morfologik('morfologik_do_wyszukiwarek.txt')
+    t = open('./' + dirname + '/TITLES','w') #what if already exists
+    document_count = 0
 
     filehandle = open(filename)
     for line in filehandle:
-        for word in re.findall(r'\w+', line):
-            try:
-                print(m[word])
-                time.sleep(1)
-            except:
-                print('>> ' + word)
+        m = re.search(r'##TITLE## (\w+)', line)
+        if m:
+            document_count += 1
+            t.write(str(document_count) + ' ' + m.group(1) + '\n' )
+        else:
+            for word in re.findall(r'\w+', line):
+                print(word + " " + str(normalize(word, m, stemmed)))
+    
+    t.close()
 
+def normalize(w, m, stemmed):
+    w = w.lower()
+    try:
+        print(w)
+        res = m[w] 
+    except TypeError:
+        res = [w]
+    except KeyError:
+        res = [w]
+    if stemmed:
+        return stem(res)
+    else:
+        return res
+
+def stem(w): #stub
+    return w
 
 generate_index('wikipedia_dla_wyszukiwarek.txt')
