04484abc1cc251a11de63dbba12db0aa2cb51f02
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-04-25 12:32:43 +0200

    more cache-like approach with morfologik, some refactoring, title file caching , file indexing to 3-letter files

--- indexer.py
+++ indexer.py
@@ -8,14 +8,19 @@ import marshal
 class Indexer:
     morphologic = {}
 
+    def index_dir_slashes(self):
+        return './' + self.index_dir + '/'
+    
     def __init__(self, index_dir = "index", compressed = False, stemmed = False):
-        self.index_dir = self.create_index_directory(index_dir, compressed, stemmed)
+        self.stemmed = stemmed
+        self.compressed = compressed
+        self.index_dir = self.create_index_directory(index_dir)
 
-    def create_index_directory(self, dirname, compressed, stemmed):
-        if compressed or stemmed:
+    def create_index_directory(self, dirname):
+        if self.compressed or self.stemmed:
             dirname = dirname + '_'
-            if compressed: dirname = dirname + 'C' 
-            if stemmed: dirname = dirname + 'S'
+            if self.compressed: dirname = dirname + 'C' 
+            if self.stemmed: dirname = dirname + 'S'
 
         if not os.path.isdir('./' + dirname + '/'):
             os.mkdir('./' + dirname + '/')
@@ -29,31 +34,48 @@ class Indexer:
         f = open(filename, 'wb')
         marshal.dump(self.morphologic, f, 2)
 
-    def initialize_morphologic(self, filename):
-        filehandle = open(filename)
-        for line in filehandle:
-            forms = line.split(' ')
-            word = forms[0]
-            self.morphologic[word] = forms[1:]
+    def initialize_morphologic(self, filename, cachefile):
+        if os.path.exists(cachefile):
+            self.load_morphologic(cachefile)
+    
+        if self.morphologic == {}:
+            filehandle = open(filename)
+            for line in filehandle:
+                forms = line.split(' ')
+                forms = list(map(lambda x: x.rstrip(), forms))
+                word = forms[0]
+                self.morphologic[word] = forms[1:]
+            self.dump_morphologic(cachefile)
 
     def index_documents(self, filename):
-        t = open('./' + self.index_dir + '/TITLES','w') #what if already exists
+        index_titles = True
+        if os.path.exists(self.index_dir_slashes() + 'TITLES'):
+            index_titles = False        
+        else:
+            t = open(self.index_dir_slashes() + 'TITLES','w')
+
         document_count = 0
+        index_count = 0
 
         filehandle = open(filename)
         for line in filehandle:
             m = re.search(r'^##TITLE## (.*)$', line)
             if m:
                 document_count += 1
-                t.write(str(document_count) + ' ' + m.group(1) + '\n' )
+                if index_titles: t.write(str(document_count) + ' ' + m.group(1) + '\n' )
             else:
-                pass
-                #for word in re.findall(r'\w+', line):
-                #    print(word + " " + str(normalize(word, m, stemmed)))
-        
+                for word in re.findall(r'\w+', line):
+                    bases = self.normalize(word)
+                    for base in bases:
+                        if len(base) >= 3:
+                            indexfilehandle = open(self.index_dir_slashes() + base[0:3], "a")
+                        else:
+                            indexfilehandle = open(self.index_dir_slashes() + 'SHORT', "a") 
+                        indexfilehandle.write(base + " " + str(document_count) + "\n")
+                        indexfilehandle.close()
         t.close()
 
-    def normalize(w, stemmed):
+    def normalize(self, w):
         w = w.lower()
  
         if w in self.morphologic:
@@ -61,7 +83,7 @@ class Indexer:
         else:
             lemated = [w]
 
-        if stemmed:
+        if self.stemmed:
             return [stem(w) for w in lemated]
         else:
             return lemated
@@ -76,13 +98,12 @@ if __name__ == "__main__":
 
     print('initializing morphologic...', end="")
     sys.stdout.flush()
-    indexer.initialize_morphologic('data/morfologik_do_wyszukiwarek.txt')
-    #indexer.load_morphologic('morphologic.pickle')
+    indexer.initialize_morphologic('data/morfologik_do_wyszukiwarek.txt', 'data/morfologik.marshal')
     print('ok')
 
-    #indexer.dump_morphologic('morphologic.pickle')
-    #print ('indexing...',)
-    #sys.stdout.flush()
-    #indexer.index_documents('data/wikipedia_dla_wyszukiwarek.txt')
+    print('running indexing...', end="")
+    sys.stdout.flush()
+
+    indexer.index_documents('data/wikipedia_dla_wyszukiwarek.txt')
     #indexer.index_documents('test.txt')
     #print ('ok')
