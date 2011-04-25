77dd57aaeaaec63c9183f2185deb5cd133a482c9
Aleksander Balicki <balicki.aleksander@gmail.com>
2011-04-25 22:08:07 +0200

    some refactoring with marshalization of data, dict generation

--- indexer.py
+++ indexer.py
@@ -23,26 +23,17 @@ class Indexer:
             os.mkdir('./' + dirname + '/')
         return dirname
 
-    def load_morphologic(self, filename):
-        f = open(filename, 'rb')
-        self.morphologic = marshal.load(f)
-
-    def dump_morphologic(self, filename):
-        f = open(filename, 'wb')
-        marshal.dump(self.morphologic, f, 2)
-
     def initialize_morphologic(self, filename, cachefile):
         if os.path.exists(cachefile):
-            self.load_morphologic(cachefile)
+            Indexer.load_dict(cachefile)
     
         if self.morphologic == {}:
             filehandle = open(filename)
             for line in filehandle:
                 forms = line.split(' ')
-                forms = list(map(lambda x: x.rstrip(), forms))
-                word = forms[0]
-                self.morphologic[word] = forms[1:]
-            self.dump_morphologic(cachefile)
+                forms[-1] = forms[-1].rstrip()
+                self.morphologic[forms[0]] = forms[1:]
+            Indexer.dump_dict(self.morphologic, cachefile)
 
     def index_documents(self, filename):
         title_path = os.path.join(self.index_dir, 'TITLES')
@@ -50,10 +41,9 @@ class Indexer:
             index_titles = False        
         else:
             index_titles = True
-            t = open(title_path, 'w')
+            title_handle = open(title_path, 'w')
 
         document_count = 0
-        index_count = 0
         regexp = re.compile(r'\w+')
 
         filehandle = open(filename)
@@ -61,9 +51,10 @@ class Indexer:
             if line[:9] == '##TITLE##':
                 if document_count % 1000 == 0:
                     print('indexed ', document_count)
+                    sys.stdout.flush()
                 document_count += 1
                 if index_titles:
-                    t.write(str(document_count) + ' ' + line[10:].strip() + '\n' )
+                    title_handle.write(str(document_count) + ' ' + line[10:].strip() + '\n' )
             else:
                 for word in regexp.findall(line):
                     bases = self.normalize(word)
@@ -76,8 +67,33 @@ class Indexer:
                         indexfilehandle = open(path, 'a') 
                         indexfilehandle.write(base + ' ' + str(document_count) + '\n')
 
+    def generate_dicts(self):
+        for i, filename in enumerate(os.listdir(self.index_dir)):
+            if i % 1000 == 0:
+                print('generated ', i)
+                sys.stdout.flush()
+
+            fh = open(os.path.join(self.index_dir, filename))
+            index_dict = {}
+            for line in fh:
+                [key, value] = line.split()
+                value = value.rstrip()
+                if key in index_dict and index_dict[key][-1] != value:
+                    index_dict[key].append(value)
+                else:
+                    index_dict[key] = [value]
+            Indexer.dump_dict(index_dict, os.path.join(self.index_dir, filename + '.marshal'))
+
+    def dump_dict(d, fn):
+        dh = open(fn, 'wb')
+        marshal.dump(d, dh, 2)
+        
+    def load_dict(fn):
+        dh = open(fn, 'rb')
+        return marshal.load(dh)
+
     def normalize(self, w):
-        w = w.lower()#.strip(' ,`!()[]{};:\'"<>.?/')
+        w = w.lower()
  
         if w in self.morphologic:
             lemated = self.morphologic[w] 
@@ -97,14 +113,17 @@ import sys
 if __name__ == "__main__":
     indexer = Indexer()
 
-    print('initializing morphologic...', end="")
+    #print('initializing morphologic...', end="")
+    #sys.stdout.flush()
+    #indexer.initialize_morphologic('data/morfologik_do_wyszukiwarek.txt', 'data/morfologik.marshal')
+    #print('ok')
+
+    #print('running indexing...')
+    #sys.stdout.flush()
+    #indexer.index_documents('data/wikipedia_dla_wyszukiwarek.txt')
+    #print('ok')
+
+    print('generating dictionaries...')
     sys.stdout.flush()
-    indexer.initialize_morphologic('data/morfologik_do_wyszukiwarek.txt', 'data/morfologik.marshal')
+    indexer.generate_dicts()
     print('ok')
-
-    print('running indexing...', end="")
-    sys.stdout.flush()
-
-    indexer.index_documents('data/wikipedia_dla_wyszukiwarek.txt')
-    #indexer.index_documents('test.txt')
-    #print ('ok')
