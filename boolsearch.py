df88c4bd0a2787896c02de684aeb5cd3ef1e54f6
Tomasz Maciejewski <ponton@jabster.pl>
2011-05-06 21:27:38 +0200

    search queries in chunks of size 1000 to avoid memory usage over 1GB

--- boolsearch.py
+++ boolsearch.py
@@ -8,46 +8,61 @@ try:
 except:
     pass
 
-i = indexer.Indexer()
+def search(s, i, num):
+    queries = []
+    results = []
+    query_words = {}
+    query_normalized_words = {}
+    eof = False
+
+    i.morfologik_cache.clear()
+    i.index_cache.clear()
+
+    for _ in range(num):
+        try:
+            query = searcher.Query(input())
+            for word in query.get_words():
+                query_words.setdefault(word[:3], set()).add(word)
+            queries.append(query)
+        except EOFError:
+            eof = True
+            break
+
+    print('normalizing')
+
+    for prefix in query_words:
+        words = query_words[prefix]
+        i.load_to_morfologik_cache(words, prefix)
+        for word in words:
+            for base in i.normalize(word):
+                query_normalized_words.setdefault(base[:3], set()).add(base)
+            
+    print('loading index')
+
+    for prefix in query_normalized_words:
+        i.load_to_index_cache(query_normalized_words[prefix], prefix)
+
+    print('searching')
+
+    for query in queries:
+        results.append(s.search(query))
+
+    i.morfologik_cache.clear()
+    i.index_cache.clear()
+
+    i.load_titles()
+
+    print('printing')
+
+    for res in results:
+        print('QUERY:', query, 'TOTAL:', len(res))
+        for doc in res:
+            print(i.get_title(doc))
+
+    return not eof
+
+i = indexer.Indexer(compressed = True)
 s = searcher.Searcher(i)
 
-queries = []
-results = []
-query_words = {}
-query_normalized_words = {}
-
-print('normalizing')
-
-while True:
-    try:
-        query = searcher.Query(input())
-        for word in query.get_words():
-            query_words.setdefault(word[:3], set()).add(word)
-        queries.append(query)
-    except EOFError:
-        break
-
-for prefix in query_words:
-    words = query_words[prefix]
-    i.load_to_morfologik_cache(words, prefix)
-    for word in words:
-        for base in i.normalize(word):
-            query_normalized_words.setdefault(base[:3], set()).add(base)
-        
-for prefix in query_normalized_words:
-    i.load_to_index_cache(query_normalized_words[prefix], prefix)
-
-for query in queries:
-    results.append(s.search(query))
-
-i.morfologik_cache.clear()
-i.index_cache.clear()
-
-i.load_titles()
-
-print('printing')
-
-for res in results:
-    print('QUERY:', query, 'TOTAL:', len(res))
-    for doc in res:
-        print(i.get_title(doc))
+while search(s, i, 1000):
+    pass
